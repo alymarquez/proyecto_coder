7069670936c3239cd61d803b44438d53bcec774e
Alina Marquez <alyym.55@gmail.com>
2023-02-03 23:39:22 -0300

    proyecto final

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from blog.models import animal
 
-# Create your tests here.
+
+class AnimalTests(TestCase):
+
+    def test_creacion_de_animal(self):
+
+        animal_nombre_valido = animal.objects.create(
+            nombre="nombre corto", tipo_de_animal ="tipo animal", edad="años"
+        )
+        self.assertEqual(animal.objects.all().count(), 1)
+        self.assertIsNotNone(animal_nombre_valido.id)
+
